008fe61cfc409e99298dea2cdfe434499956b25f
cyberfolk <andrea1995ferrari@gmail.com>
2023-07-05 15:49:47 +0200

    Print specializations in select

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,6 +1,14 @@
 <script>
+import { state } from "../state";
 export default {
   name: "SiteMain",
+  data() {
+    return { state, };
+  },
+  mounted() {
+    state.fetchSpecializations();
+  },
+
 };
 
 </script>
@@ -8,7 +16,55 @@ export default {
 <template>
   <main id="app_main" class="">
     <div class="container">
-      <h1>CIAONEEEE</h1>
+      <h3 class="mb-3 mt-5">Che specialista desideri?</h3>
+
+      <form action="{{ route('form-specialization') }}" method="post">
+        <div class="input-group mb-3">
+          <select required class="form-select form-select-lg" name="specializations" id="specializations">
+            <option disabled selected>Select one</option>
+            <option v-for="spec in state.specializations" value="{{ spec.id }}">{{ spec.name }}</option>
+          </select>
+          <button type="submit" class="input-group-text">Submit</button>
+        </div>
+
+        <!-- <div class="my-3"><small class="text-danger">{{ $message }}</small></div> -->
+      </form>
+
+      <!--       @if (isset($doctors) && !empty($doctors))
+      <div class="row">
+        @foreach ($doctors as $index => $doctor)
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h2 class="card-title m-0 text-uppercase me-3 p-0 ">{{ $users[$index] -> name }} {{ $users[$index] -> lastname }}</h2>
+              <div>
+                <p><strong>Phone number: </strong>{{ $doctor -> phone }}</p>
+              </div>
+              <div>
+                <p><strong>Email: </strong>{{ $users[$index] -> email }}</p>
+              </div>
+              <p><strong>Prestazioni: </strong>{{ $doctor -> service }}</p>
+              <p><strong>Indirizzo </strong>{{ $doctor -> address }}</p>
+              <span><strong>Specializzazioni:</strong></span>
+              <ul>
+                @forelse ($doctor->specializations as $specialization)
+                <li>{{ $specialization -> name }}</li>
+                @empty
+                <li class=" list-unstyled">nessuna specializzazione</li>
+                @endforelse
+              </ul>
+            </div>
+          </div> -->
+      <!-- /.card -->
+      <!-- </div> -->
+      <!-- /.col -->
+      <!-- @endforeach -->
+      <!-- </div> -->
+      <!-- /.row -->
+      <!--  @else
+      <div>Nessun risultato</div>
+      @endif -->
+
     </div>
     <!-- /.container -->
   </main>
--- src/state.js
+++ src/state.js
@@ -3,4 +3,22 @@ import { router } from './router';
 import axios from "axios";
 
 export const state = reactive({
+    loading_specializations: true,
+    API_URL_BASE: "http://127.0.0.1:8000/",
+    API_SPECIALIZATIONS: "api/specializations",
+    specializations: [],
+
+    fetchSpecializations() {
+        const url = this.API_URL_BASE + this.API_SPECIALIZATIONS;
+        axios
+            .get(url)
+            .then(response => {
+                this.specializations = response.data.specializations;
+                console.log(this.specializations);
+                this.loading_specializations = false;
+            })
+            .catch(error => {
+                this.error = error.message
+            })
+    },
 });
