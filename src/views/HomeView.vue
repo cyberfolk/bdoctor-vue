373872a6229a363c45d3ce6cea25273ffb786149
cyberfolk <andrea1995ferrari@gmail.com>
2023-07-08 23:54:00 +0200

    Remove goto home-view in home-view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,12 +17,10 @@ export default {
     <section id="HomeView">
         <div class="container text-center">
             <h1 class="text-center align-middle py-5">HomeView</h1>
-            <router-link :to="{ 'name': 'home' }" class="d-block">&LeftArrow; Go back to home</router-link>
 
-            <div class="py-5 d-flex gap-3 justify-content-center">
+            <div class="pb-5 d-flex gap-3 justify-content-center">
                 <div v-for="slug in slugs" class="">
-                    <router-link :to="{ name: 'doctor', params: { slug: slug } }" class="btn btn-primary">Info Doctor: {{
-                        slug }}</router-link>
+                    <router-link :to="{ name: 'doctor', params: { slug: slug } }" class="btn btn-primary">Info Doctor: {{ slug }}</router-link>
                 </div>
             </div>
             <!-- /.d-flex -->
