32a2e93aa819e80f8388c333b37e9de4d975a0df
cyberfolk <andrea1995ferrari@gmail.com>
2023-07-11 19:11:07 +0200

    Update doctor card

--- src/components/item/CardDoctor.vue
+++ src/components/item/CardDoctor.vue
@@ -1,37 +1,34 @@
 <script>
-import { state } from "./../../state";
+import { state } from '../../state';
 export default {
     name: "CardDoctor",
     data() { return { state, }; },
-    props: {
-        title: String,
-        cover: String,
-        info: String,
-        type: Object,
-        technologies: Array,
-        slug: String,
-    },
+    props: { doctor: Object, },
 };
 </script>
 <template>
-    <div class="card position-relative border-0 ms-shadow-white text-center">
-        <img v-if="cover" class="card-img ms-img-fit" :src="cover" :alt="title" />
-        <img v-if="type" width="50" class="type position-absolute top-0 end-0 mt-2 me-2"
-            :src="state.completePath(type)" :title="type.name">
-        <h3 class="title position-absolute w-100 mt-4">{{ title }}</h3>
-        <div class="card-body position-absolute bottom-0 w-100">
-            <p class="info mb-5"> {{ info?.slice(0, 150) + '...' }}
-                <router-link class="fw-bold badge bg-primary"
-                    :to="{ name: 'single-project', params: { slug: slug } }">Read More</router-link>
-            </p>
-            <div v-if="technologies">
-                <img v-for="tech in  technologies" width="50" class="technology badge bg-darkless me-1"
-                    :src="state.completePath(tech)" :title="tech.name">
+    <router-link :to="{ name: 'doctor', params: { slug: doctor.slug } }" class=" text-decoration-none border-4">
+        <div class="card h-100" style="width:18rem;">
+            <img v-if="doctor.photo != null" :src="state.getImagePath(doctor.photo)" class="card-img-top" alt="...">
+            <img v-else src="../assets/image/bdoctor.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h2 class="card-title text-uppercase">{{ doctor.name }} {{ doctor.lastname }}</h2>
+                <div class="mb-2 badge bg-danger me-2"><strong>Vote: </strong>{{ doctor.avgVote }}</div>
+                <div class="mb-2 badge bg-primary"><strong>#Reviews: </strong>{{ doctor.countReviews }}</div>
+                <div class="mb-2"><strong>Phone number: </strong>{{ doctor.phone }}</div>
+                <div class="mb-2"><strong>Email: </strong>{{ doctor.email }}</div>
+                <div class="mb-2"><strong>Prestazioni: </strong>{{ doctor.service }}</div>
+                <div class="mb-2"><strong>Indirizzo </strong>{{ doctor.address }}</div>
+                <div><strong>Specializzazioni:</strong></div>
+                <ul v-if="doctor.specializations">
+                    <li v-for="spec in doctor.specializations">{{ spec.name }}</li>
+                </ul>
+                <div v-else>Nessuna specializzazione</div>
             </div>
+            <!-- /.card-body -->
         </div>
-        <!-- /.body -->
-    </div>
-    <!-- /.card -->
+        <!-- /.card -->
+    </router-link>
 </template>
 
 <style lang="scss" scoped>
