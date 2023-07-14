0780e3ce133b19e5e4dbc72c3903f7c26f512a54
Nicowski25 <nicola.faedo0610@gmail.com>
2023-07-14 16:55:07 +0200

    #9 scss and fontawesome

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import { router } from './router.js'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 
 // Import name-icon here
 import { } from '@fortawesome/free-solid-svg-icons'
@@ -18,4 +20,4 @@ import { } from '@fortawesome/free-brands-svg-icons'
 // Insert name-icon here
 library.add()
 
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon, faStar, faFileArrowDown).mount('#app')
