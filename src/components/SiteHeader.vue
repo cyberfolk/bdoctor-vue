2c7ec94a01827f6ac2073dc00b31d55ecc6a8754
Nicowski25 <nicola.faedo0610@gmail.com>
2023-07-14 16:55:31 +0200

    #9 header button fix

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -11,11 +11,11 @@ export default {
 <template>
   <header id="app_header">
     <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow px-4">
+
+      <!-- Home button -->
       <router-link class="navbar-brand" :to="{ 'name': 'home' }">
         <img height="80" src="./../assets/image/bdoctor.png" alt="Personal Logo">
       </router-link>
-
-
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         @click="visible = !visible">
@@ -23,19 +23,22 @@ export default {
       </button>
       <!-- navbar-toggler -->
 
+      
       <div class="navbar-collapse collapse justify-content-end align-items-center" id="navbarSupportedContent">
+        <!-- Search Specialization -->
         <SearchSpecializations class="flex-grow-1 m-3"></SearchSpecializations>
+        
         <ul class="navbar-nav m-3">
           <li class="nav-item">
-            <a class="btn btn-teal text-white py-2 my-2 me-3" href="http://127.0.0.1:8000/register">Register</a>
+            <a class="ms-border-gold badge fs-6 fw-light py-3 text-white py-2 my-2 me-3 underline-n" href="http://127.0.0.1:8000/register">Register</a>
           </li>
           <li class="nav-item">
-            <a class="btn btn-indigo text-white py-2 my-2" href="http://127.0.0.1:8000/login">Login</a>
+            <a class="ms-border-gold badge fs-6 fw-light py-3 text-white py-2 my-2 underline-n" href="http://127.0.0.1:8000/login">Login</a>
           </li>
         </ul>
         <!-- </div> -->
-
       </div>
+
     </nav>
     <!-- navbar-collapse -->
   </header>
@@ -44,4 +47,9 @@ export default {
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
+
+.underline-n {
+  text-decoration: none;
+}
+
 </style>
