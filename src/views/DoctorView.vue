eb041bb14d9897abec899bc41b1492ef2a44911e
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-07-17 09:36:30 +0200

    fix_bug_preset

--- src/views/DoctorView.vue
+++ src/views/DoctorView.vue
@@ -41,25 +41,29 @@ export default {
     <section id="DoctorView">
         <div class="container">
             <div v-if="doctor" class="doc_info fs-5">
-                <h1 class="text-center align-middle fw-semibold text-uppercase pt-5 pb-3">{{ doctor.name }} {{ doctor.lastname }}</h1>
+                <h1 class="text-center align-middle fw-semibold text-uppercase pt-5 pb-3">{{ doctor.name }} {{
+                    doctor.lastname }}</h1>
                 <div class="row">
                     <div class="col-md-4 p-0">
-                        <img v-if="doctor.photo != null" :src="state.getImagePath(doctor.photo)" class="card-img-top"
-                            alt="...">
+                        <img v-if="doctor.photo != null" :src="state.getImagePath(doctor.photo)"
+                             class="card-img-top object-fit-cover h-100" alt="...">
                         <img v-else src="./../assets//image/bdoctor.png" class="card-img-top" alt="...">
                     </div>
                     <!-- /.col-4 -->
                     <div class="col-md-8 bg-light">
                         <div class="d-flex mt-3">
-                            <div class="mb-2 fs-6 me-3 fw-bold"><strong class="fs-6"><i class="fa-solid fa-star fs-5 text-gold"></i></strong> {{ doctor.avgVote ? doctor.avgVote : '-' }}</div>
-                            <div class="mb-2 badge text-bg-success fs-6"><strong class=" fs-6">N. Recensioni: </strong>{{doctor.countReviews }}</div>
+                            <div class="mb-2 fs-6 me-3 fw-bold"><strong class="fs-6"><i
+                                       class="fa-solid fa-star fs-5 text-gold"></i></strong> {{ doctor.avgVote ?
+                                           doctor.avgVote : '-' }}</div>
+                            <div class="mb-2 badge text-bg-success fs-6"><strong class=" fs-6">N. Recensioni:
+                                </strong>{{ doctor.countReviews }}</div>
                         </div>
                         <div class="mb-2"><strong>Telefono: </strong>{{ doctor.phone }}</div>
                         <div class="mb-2"><strong>Email: </strong>{{ doctor.email }}</div>
                         <div class="mb-2"><strong>Prestazioni: </strong>{{ doctor.service }}</div>
                         <div class="mb-2"><strong>Indirizzo: </strong>{{ doctor.address }}</div>
                         <div class="mb-2"><Strong>Curriculum:</Strong><a :href="state.getImagePath(doctor.cv)" download><i
-                                    class="mx-3 fs-4 text-secondary fa-solid fa-file-arrow-down"></i>Download</a></div>
+                                   class="mx-3 fs-4 text-secondary fa-solid fa-file-arrow-down"></i>Download</a></div>
                         <div class="mb-2"><strong>Specializzazioni:</strong></div>
                         <ul v-if="doctor.specializations">
                             <li v-for="spec in doctor.specializations">{{ spec.name }}</li>
@@ -102,6 +106,4 @@ export default {
 
 ul {
     columns: 3;
-}
-
-</style>
+}</style>
