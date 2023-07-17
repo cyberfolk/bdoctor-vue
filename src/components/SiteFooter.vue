658c20791a90ee8b1a807acc41b6ae8aa67e6778
cyberfolk <andrea1995ferrari@gmail.com>
2023-07-17 09:33:01 +0200

    Update footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -5,12 +5,16 @@ export default {
 </script>
 
 <template>
-  <footer id="app_footer">
+  <footer id="app_footer" class="mt-5 py-5 bg-dark ">
+
+    <div class="text-center text-white">
+      <div> Andrea Ferrari | Daniele Morello | Martina De Rose | Riccardo Castiglione | Nicola Faedo | Boolean 2023 | &copy; powered by Team-2 Class #91</div>
+    </div>
+
   </footer>
   <!-- /#app_footer -->
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
-
 </style>
